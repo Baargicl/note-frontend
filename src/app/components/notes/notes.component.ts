1db8981f839a15f6143b1415aafc14bb10c94b16
Ariano Ballarini <ariano.ballarini.1@postfinance.ch>
2024-06-27 17:06:38 +0200

    added dialog popup

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -12,6 +12,8 @@ export class NotesComponent implements OnInit {
 
   allNotes: Note[] = []
 
+
+
   constructor(private noteService: NoteService,
               private router: Router) {
   }
@@ -20,9 +22,25 @@ export class NotesComponent implements OnInit {
     this.noteService.getAllStuff().subscribe(response => {
       this.allNotes = response;
     })
+
+
+
+    const showButton = document.getElementById("openDialogButton");
+
+
+    const dialog = document.getElementById('dialog') as HTMLDialogElement;
+    const closeButton = document.getElementById('close');
+    if (showButton && dialog && closeButton ) {
+      showButton.addEventListener('click', () => dialog.showModal());
+      closeButton.addEventListener('click', () => dialog.close());
+    }
+
   }
 
   openNote(id: number) {
     this.router.navigate(['notes/'+ id])
   }
+
+
 }
+
